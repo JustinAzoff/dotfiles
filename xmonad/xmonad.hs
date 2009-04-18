95e6ec730b701e7cd13f897e2bbebe64d6e8368d
Justin <justin@bouncybouncy.net>
2009-04-18 12:24:35 -0400

    added twitter binding

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -36,10 +36,11 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
      ,((modm, xK_F2), spawn $ XMonad.terminal conf)
  
-    -- launch dmenu
+    --launch commands
     --, ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu -b` && eval \"exec $exe\"")
     , ((modm,               xK_p     ), shellPrompt defaultXPConfig)
     , ((modm .|. shiftMask, xK_p     ), prompt ("xterm" ++ " -e") defaultXPConfig)
+    , ((modm .|. shiftMask, xK_t     ), prompt ("twyt" ++ " tweet") defaultXPConfig)
  
     -- launch gmrun
     --, ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
