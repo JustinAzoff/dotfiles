cb227d2ca6b450b7aa96d0757295e2701eccdb75
Justin Azoff <justin@bouncybouncy.net>
2011-12-13 18:39:18 -0500

    more vim updates

--- link.sh
+++ link.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 #vim
 ln -s ~/projects/dotfiles/vim/vimrc.minimal ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
+    curl -so ~/.vim/autoload/pathogen.vim \
+        https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+fi
+
+mkdir -p ~/.vim/_backup  ~/.vim/_temp
 
 #xmonad
 mkdir -p ~/.xmonad
