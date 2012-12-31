d9f3662c2fcc1fdb5d538f5f5b333a16d1df0edf
Justin <justin@x230.(none)>
2012-12-30 20:55:19 -0500

    tweaks

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -31,6 +31,7 @@ modkey = "Mod4"
 layouts =
 {
     awful.layout.suit.max,
+    awful.layout.suit.max.fullscreen,
     awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
@@ -40,7 +41,6 @@ layouts =
     -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier
 }
 -- }}}
