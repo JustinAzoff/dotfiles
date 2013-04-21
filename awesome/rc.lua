ced5174463463e2261291dc437146809eb5d62d9
Justin <justin@x230.(none)>
2013-04-20 21:12:55 -0400

    updates

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -348,6 +348,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { class = "Steam" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
