077145222f934f7827abde1e7d1552817471ba1d
Justin Azoff <justin@bouncybouncy.net>
2013-08-13 22:24:21 -0400

    switch theme

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -13,7 +13,7 @@ require("debian.menu")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "gnome-terminal"
