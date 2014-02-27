e4c4153881f2b61f4662f5f9796b0c5b79819dab
Justin Azoff <justin@bouncybouncy.net>
2014-02-26 19:58:42 -0500

    add props for tilda and pithos

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -350,6 +350,12 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "Steam" },
       properties = { floating = true } },
+    { rule = { class = "tilda" },
+      properties = { floating = true } },
+    { rule = { class = "Tilda" },
+      properties = { floating = true } },
+    { rule = { class = "Pithos" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
